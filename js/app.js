2b6df2ec5607ec4c10687b9112a5ca99b3baa0c1
Nhu Trinh <nqtrinh87@gmail.com>
2019-07-14 19:13:34 -0700

    pagination works, keywords populate and work

--- js/app.js
+++ js/app.js
@@ -1,35 +1,79 @@
 'use strict';
 
-let dataFile = 'data/page-1.json';
+const filePath1 = 'data/page-1.json';
+const filePath2 = 'data/page-2.json';
+const dataFile1 = 'page-1';
+const dataFile2 = 'page-2';
 
-function PhotoObj(photo) {
+function PhotoObj(photo, file) {
   this.image_url = photo.image_url;
   this.title = photo.title;
   this.description = photo.description;
   this.keyword = photo.keyword;
   this.horns = photo.horns;
+  this.file = file;
 }
 
 PhotoObj.allPhotos = [];
 
-PhotoObj.prototype.renderWithHandleBars = function(){
-  const source = $('#photo-template').html();
-  const template = Handlebars.compile(source);
-  const html = template(this);
-  $('#photos').append(html);
+PhotoObj.prototype.renderWithHandleBars = function(file){
+  if(file === dataFile1){
+    PhotoObj.allPhotos.filter(file => file === this.file)
+    const source = $('#photo-template').html();
+    const template = Handlebars.compile(source);
+    const html = template(this);
+    $('#photos').append(html);
+  } 
+  else if(file === dataFile2){
+    PhotoObj.allPhotos.filter(file => file === this.file)
+    const source = $('#photo-template').html();
+    const template = Handlebars.compile(source);
+    const html = template(this);
+    $('#photos').append(html);
+  }
 };
 
 
-PhotoObj.readJson = () => {
-  $.get(dataFile, 'json')
+PhotoObj.makeAnimal = function(data, file){
+  data.forEach(animal => {
+    PhotoObj.allPhotos.push(new PhotoObj(animal, file));
+  })
+}
+
+
+PhotoObj.readJson = (filePath) => {
+  if(filePath === filePath1){
+    $.get(filePath1, 'json')
+      .then(data => {
+        PhotoObj.makeAnimal(data, dataFile1)
+      })
+      .then(PhotoObj.loadPhotos)
+      .then(PhotoObj.loadKeyWords)
+
+  } else if (filePath === filePath2){
+    $.get(filePath2, 'json')
+      .then(data => {
+        PhotoObj.makeAnimal(data, dataFile2)
+      })
+      .then(PhotoObj.loadPhotos)
+      .then(PhotoObj.loadKeyWords)
+
+  } else {
+    $.get(filePath1, 'json')
+      .then(data => {
+        PhotoObj.makeAnimal(data, dataFile1)
+      })
+
+    $.get(filePath2, 'json')
     .then(data => {
-      data.forEach(p => {
-        PhotoObj.allPhotos.push(new PhotoObj(p));
-      });
+      PhotoObj.makeAnimal(data, dataFile2)
     })
-    .then(PhotoObj.loadPhotos);
+    .then(PhotoObj.loadPhotos)
+    .then(PhotoObj.loadKeyWords)
+  }
 };
 
+// 
 PhotoObj.loadPhotos = () => {
   let sortedPhotos = PhotoObj.allPhotos.sort((a, b) => {
     if(a.title.toUpperCase() < b.title.toUpperCase()){
@@ -40,13 +84,40 @@ PhotoObj.loadPhotos = () => {
       return 1;
     }
   })
-  sortedPhotos.forEach(photo => photo.renderWithHandleBars());
+  console.log(PhotoObj.allPhotos)
+  sortedPhotos.forEach(photo => {
+    let file = photo.file
+    photo.renderWithHandleBars(file)
+  })
 };
 
-$(() => PhotoObj.readJson());
+PhotoObj.loadKeyWords = () => {
+  PhotoObj.allPhotos.forEach(photo => {
+    $('#creatures').append('<option class="keyword"></option>');
+    let optionClone = $('option[class="keyword"]');
+    optionClone.html(photo.keyword);
+    optionClone.attr('class', photo.keyword);
+    optionClone.attr('value', photo.keyword);
+  })
+}
 
 $('select[name="creatures"]').on('change', function(){
   let $selection = $(this).val();
   $('section').hide(); 
-  $(`section[class="${$selection}"]`).show();
+  $(`section[class="${$selection} page-photos"]`).show();
 });
+
+$(() => PhotoObj.readJson());
+
+$('#page-two').on('click', function(){
+  PhotoObj.allPhotos = [];
+  $('.page-photos').remove(); 
+  PhotoObj.readJson(filePath2)
+})
+
+$('#page-one').on('click', function(){
+  PhotoObj.allPhotos = [];
+  $('.page-photos').remove(); 
+  PhotoObj.readJson(filePath1)
+})
+
